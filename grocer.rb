94f965603c12275c5494d778786b854d08b45b0b
Kevin Karma <kevinkarma55@gmail.com>
2019-08-15 00:25:50 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,48 @@
 def consolidate_cart(cart)
-  # code here
+  groceries_updated = cart.inject({}) do |consolidated, grocery|
+    item = grocery.keys.first
+    consolidated[item] ||= grocery[item].merge(count: 0)
+    consolidated[item][:count] += 1
+    consolidated
+  end
 end
 
-def apply_coupons(cart, coupons)
-  # code here
+def apply_coupons(cart, coupons) 
+  coupons.each do |coupon| 
+    coupon.each do |attribute, value| 
+      name = coupon[:item] 
+      if cart[name] && cart[name][:count] >= coupon[:num] 
+        if cart["#{name} W/COUPON"] 
+          cart["#{name} W/COUPON"][:count] += coupon[:num] 
+        else 
+          cart["#{name} W/COUPON"] = {:price => coupon[:cost]/coupon[:num], 
+          :clearance => cart[name][:clearance], :count => coupon[:num]} 
+        end 
+        cart[name][:count] -= coupon[:num] 
+      end 
+    end 
+  end 
+  cart 
 end
 
 def apply_clearance(cart)
-  # code here
+  cart.each do |item,value|
+    if value[:clearance] 
+      value[:price] = (value[:price] * 0.8).round(2)
+    end 
+  end
+  cart
 end
 
 def checkout(cart, coupons)
-  # code here
+  total = 0 
+  result = apply_clearance(apply_coupons(consolidate_cart(cart),coupons))
+  result.each do |item, value|
+    total += result[item][:price] * result[item][:count]
+  end
+  if total > 100 
+    return total *= 0.90 
+  else
+   return total
+  end
 end
